e2b9b70fcbd36fed8428ae4aa7f23ef70fc1c791
RicardoRQuez <ricardo88812968@gmail.com>
2023-10-10 23:37:54 +0000

    actualiacion

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
+const prompt = require('prompt-sync')();
 
 //No cambiar el nombre de la función ordenarNumeros
 
+const numero1 = parseInt(prompt("Ingrese el primer número: "));
+const numero2 = parseInt(prompt("Ingrese el segundo número: "))
+const numero3 = parseInt(prompt("Ingrese el tercer número: "))
+
 function ordenarNumeros(num1, num2, num3) {
-
-  let mayor
-  let centro
-  let menor
-
-
-
+  let mayor = Math.max(num1, num2, num3);
+  let menor = Math.min(num1, num2, num3);
+  let centro = num1 + num2 + num3 - mayor - menor
+    
 
   
+
 // No modificar el código debajo de esta línea
   return {
     mayor,
@@ -20,5 +23,11 @@ function ordenarNumeros(num1, num2, num3) {
     ordenMenorAMayor: [menor, centro, mayor],
   };
 }
+const resultado = ordenarNumeros(numero1, numero2, numero3);
+
+console.log(resultado.ordenMayorAMenor);
+console.log(resultado.ordenMenorAMayor);
+
+
 
 module.exports = ordenarNumeros;
